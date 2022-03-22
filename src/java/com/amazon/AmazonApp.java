37d09ac0f74eb31df54c1b51378d4b9a88d45282
Dani <amaximoaiei.l@yahoo.com>
2022-03-22 21:34:38 +0200

    --

--- src/java/com/amazon/AmazonApp.java
+++ src/java/com/amazon/AmazonApp.java
@@ -1,14 +1,11 @@
 package com.amazon;
 
 import com.amazon.ui.LandingPageUI;
-import com.amazon.ui.ProductUI;
 import com.amazon.ui.SellerUI;
 
 import java.io.IOException;
 
 public class AmazonApp {
-
-    public static ProductUI product;
 
     public static void main(String[] args) throws IOException {
         LandingPageUI landingPageUI = new LandingPageUI();
